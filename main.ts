72f72f67980f90a1a007e73d2d75d7db709d18e7
HevoliB <115397467+HevoliB@users.noreply.github.com>
2022-11-22 18:52:28 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,22 @@
 input.onButtonPressed(Button.A, function () {
     led.unplot(xposition, 0)
-    xposition += 1
+    xposition += -1
     led.plot(xposition, 0)
 })
 input.onButtonPressed(Button.B, function () {
     led.unplot(xposition, 0)
-    xposition += -1
+    xposition += 1
+    led.plot(xposition, 0)
 })
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.clearScreen()
+    yposition += 1
+    xposition = 0
+    led.plot(xposition, yposition)
+})
+let yposition = 0
 let xposition = 0
 basic.clearScreen()
 xposition = 0
+yposition = 0
 led.plot(0, 0)
