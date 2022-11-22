f99d2d06376538625a499fef88bb77341b22c098
HevoliB <115397467+HevoliB@users.noreply.github.com>
2022-11-22 19:08:17 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,19 @@
 input.onButtonPressed(Button.A, function () {
-    led.unplot(xposition, 0)
+    led.unplot(xposition, yposition)
     xposition += -1
-    led.plot(xposition, 0)
+    led.plot(xposition, yposition)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("Super Papa!")
 })
 input.onButtonPressed(Button.B, function () {
-    led.unplot(xposition, 0)
+    led.unplot(xposition, yposition)
     xposition += 1
-    led.plot(xposition, 0)
+    led.plot(xposition, yposition)
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     basic.clearScreen()
     yposition += 1
-    xposition = 0
     led.plot(xposition, yposition)
 })
 let yposition = 0
