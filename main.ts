193eabe12620590883474207607548b61bbbcde8
HevoliB <115397467+HevoliB@users.noreply.github.com>
2022-12-01 18:47:21 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,25 @@
 input.onButtonPressed(Button.A, function () {
-    led.unplot(xposition, yposition)
-    xposition += -1
-    led.plot(xposition, yposition)
+    basic.showIcon(IconNames.Heart)
 })
 input.onButtonPressed(Button.AB, function () {
-    basic.showString("Super Fille")
+    basic.showLeds(`
+        # . # . #
+        . # # # .
+        # # # # #
+        . # # # .
+        # . # . #
+        `)
 })
 input.onButtonPressed(Button.B, function () {
-    led.unplot(xposition, yposition)
-    xposition += 1
-    led.plot(xposition, yposition)
+    basic.showString("\"clap clap clap\"")
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     basic.clearScreen()
-    yposition += 1
-    led.plot(xposition, yposition)
 })
-let yposition = 0
-let xposition = 0
-basic.clearScreen()
-xposition = 0
-yposition = 0
-led.plot(xposition, yposition)
+basic.showLeds(`
+    # . . # .
+    . # # . .
+    . # # . .
+    # . . # .
+    . . . . #
+    `)
